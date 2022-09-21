1ca31adc5529a5b4973b396ba3dde59202325cf5
Dmitiy <dimapermyakov55@gmail.com>
2022-09-22 00:02:54 +0300

    The lab-03 was started

---
+++ Term-3/Базовые компонентв интернет технологий/lab-03/lab_python_fp/unique.py
@@ -0,0 +1,29 @@
+# Copyright 2022 Dmitriy <dimapermyakov55@gmail.com>
+
+
+# Итератор для удаления дубликатов
+class Unique(object):
+    def __init__(self, items, ignore_case=False, **kwargs):
+        self._items = items
+        self._dict = kwargs
+        self._ignore_case = ignore_case
+        # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
+        # в зависимости от значения которого будут считаться одинаковыми строки в разном регистре
+        # Например: ignore_case = True, Aбв и АБВ - разные строки
+        #           ignore_case = False, Aбв и АБВ - одинаковые строки, одна из которых удалится
+        # По-умолчанию ignore_case = False
+
+    def __next__(self):
+
+        return self._dict
+
+    def __iter__(self):
+        return self
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
